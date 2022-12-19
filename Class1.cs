b3ecc25957b40ed9b001752fad2859d7cf118a42
RidgeSpoon <119254838+RidgeSpoon@users.noreply.github.com>
2022-12-19 15:30:13 -0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity_10
+{
+    class Set
+    {
+        private List<int> elements;
+
+
+        public Set()
+        {
+            elements = new List<int>();
+        }
+
+        public bool addElement(int val)
+        {
+            if (containsElement(val)) return false;
+            else
+            {
+                elements.Add(val);
+                return true;
+            }
+        }
+
+        private bool containsElement(int val)
+        {
+            for (int i = 0; i < elements.Count; i++)
+            {
+                //If the first value is not equal to a specific value then the automatic value would be false
+                if (val == elements[i])
+                    return true;
+                // else
+                //  return false;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            foreach (int i in elements)
+            {
+                str += i + " ";
+            }
+            return str;
+        }
+
+
+        public void clearSet()
+        {
+            elements.Clear();
+        }
+
+        // Created a new set in order to add new values and leave set A unchanged 
+        public Set union(Set rhs)
+        {
+            Set C = new Set();
+            for (int i = 0; i < rhs.elements.Count; i++)
+            {
+                C.addElement(rhs.elements[i]);
+                // this.addElement (rhs.elements[i]);
+            }
+            for (int i = 0; i < this.elements.Count; i++)
+            {
+                C.addElement(this.elements[i]);
+                // this.addElement (rhs.elements[i]);
+            }
+            return C;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿//Corrected by Ridge D Witherspoon
+//Ridge D Witherspoon CST-150 12/18/2022
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Here I found a fundamental error when creating a set object. It is necessary
+            //to attach a namespace from where to pull the information from. 
+            //"make some sets"
+            //Set A = new Set();
+            //Set B = new Set();
+            Activity_10.Set A = new Activity_10.Set();
+            Activity_10.Set B = new Activity_10.Set();
+
+            //put some stuff in the sets
+            Random r = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                A.addElement(r.Next(4));
+                B.addElement(r.Next(12));
+            }
+
+            //display each set and the union
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+            Console.WriteLine("A union B: " + A.union(B));
+
+            //display original sets (should be unchanged)
+            Console.WriteLine("After union operation");
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+
+        }
+    }
+}
